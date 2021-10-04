fe0d82fce458b471d4d63a085ca82a9cbe812e65
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2021-10-04 23:17:36 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import HomePage from './HomePage'
 import PageNotFound from './PageNotFound'
 import StudySetPage from './StudySetPage'
 import RequireLoginPage from './RequireLoginPage'
-import CEstudySetPage from './CEstudySetPage'
+import CEstudySetPage from './CeStudySetPage'
 
 export {
     HomePage,
