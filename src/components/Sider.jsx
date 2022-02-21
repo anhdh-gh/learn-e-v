40d283a2a96f18e508c178fd836b5f0c25dbbb8e
Đỗ Hùng Anh <dohunganh27092000@gmail.com>
2022-02-21 16:16:07 +0700

    Update Sider.jsx

--- src/components/Sider.jsx
+++ src/components/Sider.jsx
@@ -23,7 +23,7 @@ const Sider = (props) => {
 
     useEffect(()=> {
         window.addEventListener('resize', handleResize)
-        return () => window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
     }, [])
 
     return <>
